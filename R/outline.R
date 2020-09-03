ac5ede1ce98843ab3f6b54ede49d1a3d8dad22cf
Samet Sökel <64185287+sametsoekel@users.noreply.github.com>
2020-09-03 17:19:03 +0300

    Add files via upload

---
+++ R/outline.R
@@ -0,0 +1,21 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+outline <- function(df){
+
+}
+
+
+setwd("./man")
+document()
+
+
+roxygen2
+
+getwd()

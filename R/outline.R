80a918ee2ed1a94612fce4b5b788b4c78c046c88
Samet Sökel <64185287+sametsoekel@users.noreply.github.com>
2020-09-03 20:25:11 +0300

    Add files via upload

--- R/outline.R
+++ R/outline.R
@@ -1,14 +1,35 @@
 #' A Cat Function
 #'
 #' This function allows you to express your love of cats.
-#' @param love Do you love cats? Defaults to TRUE.
+#' @param data Do you love cats? Defaults to TRUE.
 #' @keywords cats
 #' @export
 #' @examples
-#' cat_function()
+#' outline(Hitters,plot = TRUE)
 
-outline <- function(df){
-  print("za")
+
+outline <- function(data){
+
+  df <- data.frame(data)
+
+  number_obs <- length(df[,1])
+
+  missing_obs_ratio <- apply(df, 1, function(x) sum(is.na(x)) / length(x))
+
+  names(missing_obs_ratio) <- NULL
+
+  missing_var_ratio <- apply(df, 2, function(x) sum(is.na(x)) / length(x))
+
+  names(missing_var_ratio) <- NULL
+
+
+
+
 }
+
+
+
+
+
 
 

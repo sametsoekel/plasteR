0ae145964a2be1d4f94e11321dbc3be8a75b6e73
sametsoekel <64185287+sametsoekel@users.noreply.github.com>
2020-09-06 18:47:45 +0300

    outlier.outline function simply created

---
+++ R/outlier.outline.R
@@ -0,0 +1,16 @@
+#' Outlier Outline
+#'
+#' This function allows you a report about outliers of your data.
+#' @param data Dataset, matrix or dataframe.
+#' @param plot_show Logical argument for plotting outliers.
+#' @keywords outlier
+#' @export
+#' @examples
+#' outlier.outline(Hitters,plot_show = TRUE)
+
+outlier.outline <- function(data){
+  data
+}
+
+
+

313e7086da70dc7ba0e3ed7ae94092efc97e8400
sametsoekel <64185287+sametsoekel@users.noreply.github.com>
2020-09-09 16:19:14 +0300

    multivarite outlier detection added

--- R/outlier.outline.R
+++ R/outlier.outline.R
@@ -1,16 +1,22 @@
 #' Outlier Outline
 #'
-#' This function allows you a report about outliers of your data.
+#' This function allows you a report about outliers and normal distribution checks of your data.
 #' @param data Dataset, matrix or dataframe.
-#' @param plot_show Logical argument for plot.
-#' @keywords outlier
+#' @param plot_show Logical argument for plotting.
+#' @param type Default "den", Logical argument for plot type for each numeric variable. boxplot ("box"), density ("den").
+#' @param get_skew if TRUE prints each skewness coefficient for each variable on the plot
+#' @param get_normality if TRUE prints results of Shapiro-Wilk normality test for each variable on the plot
+#' @param return_clean_data if TRUE the object of outlier.outline function includes a dataframe without multivarite outliers (via robust principal component analysis)
+#' @keywords outlier, boxplot, density
 #' @export
 #' @examples
-#' outlier.outline(Hitters,plot_show = TRUE)
+#' outlier.outline(Hitters,plot_show = TRUE,type="box",get_skew = TRUE,get_normality = FALSE,
+#' return_clean_data = TRUE)
 
 
 
-outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE){
+outlier.outline <- function(data,plot_show = TRUE,type="den",get_skew=TRUE,get_normality=TRUE,
+                            return_clean_data=TRUE){
 
 
   ####### margin arbiter func.-start ########
@@ -45,7 +51,7 @@ outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE)
 #########################################################################################################
 
 
-  ####### some statistical calculations via MVN ######
+  ####### some statistical calculations via MVN and mvoutlier ######
 
   options(warn=-1) ## warn hider
 
@@ -53,12 +59,16 @@ outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE)
 
   dfn <- keep(df,is.numeric)
 
+  dfn_nona <- na.omit(dfn)
+
+  results_mvo<-aq.plot(keep(dfn_nona,is.numeric))
+
   if(nrow(dfn)>=5000 || nrow(dfn)<3) {
     stop("Sample size should be between 3 and 5000")}
 
   results <- mvn(dfn)
 
-  var_names <- variable.names(dfn)
+  var_names <- variable.names(dfn,showOutliers=TRUE)
 
   skewness <- results$Descriptives$Skew
 
@@ -66,7 +76,9 @@ outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE)
 
   normality_p_value <- results$univariateNormality$`p value`
 
-  ####### some statistical calculations via MVN ######
+  mv_outliers <- results_mvo$outliers
+
+  ####### some statistical calculations via MVN and mvoutlier #######
 
 
   #########################################################################################################
@@ -104,28 +116,51 @@ outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE)
 
       #####
       lenx <- max(x[[var]],na.rm = T)-min(x[[var]],na.rm = T)
-      leny <- den$bw
-
 
       lim1 <- min(x[[var]],na.rm = T)-lenx
       lim2 <- max(x[[var]],na.rm = T)+lenx
 
       ####### finding the p value and skewness indexes ########
 
-      text_vector <- c("Skewness Coef.:",toString(skw),
-                       "Normality Test :",toString(ntest))
+
+
+
+
+      ################# checking given logical arguments ######################
+
+      if(get_skew==T){
+        skw_txt <- c("Skewness Coef.:",toString(skw))
+      }
+      else{
+        skw_txt <- c("","")
+      }
+
+      if(get_normality==T){
+        nrm_txt <- c("Normality Test :",toString(ntest))
+      }
+      else{
+        nrm_txt <- c("","")
+      }
+
+
+
+      text_vector <- c(skw_txt,nrm_txt)
 
       color_vector <- c("dodgerblue","dodgerblue",
                         "darkblue","darkblue")
+
+      ################# checking given logical arguments ######################
+
 
 
 
       if (!anyNA(x[[var]])){
         plot(den,
-             main = var,
+             main = c(var),
              xlab = "",
              ylab = "",
-             xlim=c(lim1,lim2))
+             xlim=c(lim1,lim2),
+             col.main=c("black"))
         legend("topright",text_vector,
           bty = "n",cex = .9,text.col = color_vector)
       }
@@ -133,10 +168,11 @@ outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE)
 
 
         plot(den,
-             main = paste(var," (including NA's)"),
+             main = c(var," (including NA's)"),
              xlab = "",
              ylab = "",
-             xlim=c(lim1,lim2))
+             xlim=c(lim1,lim2),
+             col.main="red")
         legend("topright",text_vector,
           bty = "n",cex = .9,text.col = color_vector)
 
@@ -153,7 +189,13 @@ outlier.outline <- function(data,plot_show = TRUE,type="density",get_skew=FALSE)
 
   ######## top plots ########
 
+  return(
+    list(
+      "normality_p_values"=normality_p_value,
+      "without_mv_outliers"=ifelse(return_clean_data==T,dfn_nona[!mv_outliers,],"NULL")
+    )
+  )
+
 
 }
-
 
